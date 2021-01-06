9adc50d972e2aad4a1bdc2aee1522f8416e63e9a
Skejven11 <Zysku23456@gmail.com>
2021-01-06 16:51:14 +0100

    6th commit - Single castle view

--- src/com/castleproject/castle/CastleViewBB.java
+++ src/com/castleproject/castle/CastleViewBB.java
@@ -1,25 +1,51 @@
 package com.castleproject.castle;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+
+import java.io.IOException;
+
+import javax.ejb.EJB;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
 import com.castle.entities.Castle;
+import com.castle.dao.CastleDAO;
 
 @Named
 @RequestScoped
 public class CastleViewBB {
 	private static final String PAGE_CASTLE = "castleView?faces-redirect=true";
 	
-		Castle castle;
+		private Castle castle = new Castle();
+		private String ruin;
+		private int score;
 		
-		public Castle getCastle()
-		{
+		public String getRuin() {
+			return ruin;
+		}
+		
+		public Castle getCastle() {
 			return castle;
 		}
-	
-	public String viewCastle(Castle castlee)
-	{
-		castle = castlee;
-		return PAGE_CASTLE;
-	}
+		
+		public int getScore() {
+			return score;
+		}
+		
+		@Inject
+		FacesContext context;
+		
+		@EJB
+		CastleDAO castleDAO;
+		
+		public void onLoad() throws IOException{
+			castle = castleDAO.find(castle.getIdcastle());
+			if (castle.getIsRuin()==0) ruin = "Well maintained";
+			else ruin = "Ruins";
+			score = 6;
+			
+		}
 
 }
